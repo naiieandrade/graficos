b0a58c28eedfded7aa024a98f90492e73c98870f
naiieandrade <andradenaiara@hotmail.com>
2018-02-03 23:24:57 -0200

    [ADD] grafico funcionando

---
+++ chart/models.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.name
